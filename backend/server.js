8d4c16a18a7b4a7445ec74a71a8c315f2bc14c7e
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-05-07 08:39:22 +0530

    bug fixes and code optimization

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = runApp();
 
 (async () => {
   // Config
-  if (process.env.NODE_ENV !== "PRODUCTION") {
+  if (process.env.NODE_ENV !== "production") {
     require("dotenv").config({
       path: "backend/config/config.env",
     });
